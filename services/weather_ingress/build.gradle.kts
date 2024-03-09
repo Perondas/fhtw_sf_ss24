3aebb88615e3e4dbe3d53de8f5ca05bbefb2b4c3
Perondas <thomas@perondas.dev>
2024-03-09 15:05:29 +0100

    Tried another generator

--- services/weather_ingress/build.gradle.kts
+++ services/weather_ingress/build.gradle.kts
@@ -1,13 +1,23 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.swagger.codegen.v3.DefaultGenerator
+import io.swagger.codegen.v3.config.CodegenConfigurator
+import io.swagger.v3.parser.OpenAPIV3Parser
 
 val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    //id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
     application
 }
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.swagger.codegen.v3:swagger-codegen-maven-plugin:3.0.54")
+    }
+}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
@@ -27,6 +37,32 @@ application {
     mainClass.set("at.fhtw.MainKt")
 }
 
+val apiPackage   = "com.message.api"
+val modelPackage = "com.message.model"
+val ignoreFile = "${rootDir}/.swagger-codegen-ignore"
+val swaggerFile  = "${rootDir}/src/main/resources/messenger-service.yml"
+val generatedOutput = "$projectDir/build/"
+
+tasks.create("generateServer") {
+    doLast {
+        val config = CodegenConfigurator()
+        config.lang = "kotlin-client"
+        config.apiPackage = "at.fhwt.api"
+        config.modelPackage = "at.fhwt.model"
+        config.inputSpecURL = "https://dataset.api.hub.geosphere.at/v1/openapi.json"
+        config.outputDir = "${layout.buildDirectory.get()}/generated"
+        config.invokerPackage = "at.fhwt.invoker"
+
+        val something = config.toClientOptInput()
+        something.openAPI(OpenAPIV3Parser().read("https://dataset.api.hub.geosphere.at/v1/openapi.json"))
+
+        DefaultGenerator().opts(something).generate()
+    }
+}
+
+kotlin.sourceSets["main"].kotlin.srcDir("${layout.buildDirectory.get()}/generated/src/main/kotlin")
+
+
 repositories {
     mavenCentral()
 }
@@ -34,11 +70,9 @@ repositories {
 dependencies {
     implementation("org.apache.kafka:kafka-clients:3.7.0")
 
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-logging:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation( "com.squareup.moshi:moshi-adapters:1.15.1")
 
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     implementation("ch.qos.logback:logback-classic:1.4.14")
--- services/weather_ingress/src/main/kotlin/Main.kt
+++ services/weather_ingress/src/main/kotlin/Main.kt
@@ -1,42 +1,13 @@
 package at.fhtw
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.*
+import at.fhwt.api.CurrentApi
+import at.fhwt.api.MetadataApi
+import at.fhwt.model.Bundesland
+import at.fhwt.model.StationCurrentMetadataModel
 
 
-suspend fun main() {
-    val client = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json()
-        }
-    }
+fun main() {
+    val meta = MetadataApi().currentStationMetadataStationCurrentResourceIdMetadataGet("tawes-v1-10min")
 
-    val res = client.get("https://dataset.api.hub.geosphere.at/v1/station/current/tawes-v1-10min/metadata")
-    val metadata = res.body<JsonObject>()
-    val stations = metadata["stations"]?.jsonArray
-
-
-    val tl = client.get {
-        url("https://dataset.api.hub.geosphere.at/v1/station/current/tawes-v1-10min")
-        parameter("parameters", "TL")
-        for (station in stations!!) {
-            val stationId = station.jsonObject["id"]?.jsonPrimitive?.content
-            parameter("station_ids", stationId)
-        }
-    }.call.body<JsonObject>()
-
-    val features = tl["features"]?.jsonArray
-
-    for (property in features!!.map { it.jsonObject["properties"]?.jsonObject }) {
-        val stationId = property?.get("station")?.jsonPrimitive?.content
-        val temp = property?.get("parameters")?.jsonObject?.get("TL")?.jsonObject
-
-        val stationName = metadata["stations"]?.jsonArray?.find { it.jsonObject["id"]?.jsonPrimitive?.content == stationId }?.jsonObject?.get("name")?.jsonPrimitive?.content
-        println("Station $stationName has temperature $temp")
-    }
+    println(meta)
 }
