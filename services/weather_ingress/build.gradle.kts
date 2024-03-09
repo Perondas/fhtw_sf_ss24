db5c04929cfe713ef59addd4e34d0f8323392b1d
Perondas <thomas@perondas.dev>
2024-03-09 11:06:28 +0100

    Manually implemented weather gathering

--- services/weather_ingress/build.gradle.kts
+++ services/weather_ingress/build.gradle.kts
@@ -4,7 +4,7 @@ val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
-    id("org.openapi.generator") version "7.3.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
     application
 }
 
@@ -20,32 +20,11 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
     }
 }
 
-
 group = "at.fhtw"
 version = "1.0-SNAPSHOT"
 
 application {
     mainClass.set("at.fhtw.MainKt")
-}
-
-val apiRootName = "at.fhtw.geosphere.api.client"
-val generatedSourcesPath = "${layout.buildDirectory.get()}/generated"
-
-
-openApiGenerate {
-    generatorName.set("kotlin")
-    remoteInputSpec.set("https://validator.swagger.io/validator/openapi.json")
-    outputDir.set(generatedSourcesPath)
-    apiPackage.set("$apiRootName.api")
-    invokerPackage.set("$apiRootName.invoker")
-    modelPackage.set("$apiRootName.model")
-    skipValidateSpec.set(true)
-}
-
-kotlin.sourceSets["main"].kotlin.srcDir("$generatedSourcesPath/src/main/kotlin")
-
-tasks.withType<KotlinCompile>().configureEach {
-    dependsOn("openApiGenerate")
 }
 
 repositories {
@@ -55,8 +34,11 @@ repositories {
 dependencies {
     implementation("org.apache.kafka:kafka-clients:3.7.0")
 
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
 
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     implementation("ch.qos.logback:logback-classic:1.4.14")
--- services/weather_ingress/src/main/kotlin/Main.kt
+++ services/weather_ingress/src/main/kotlin/Main.kt
@@ -1,10 +1,42 @@
 package at.fhtw
 
-import at.fhtw.geosphere.api.client.api.ValidatorApi
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.*
 
 
 suspend fun main() {
-    val k =ValidatorApi()
-   val res =  k.validateByUrl("https://validator.swagger.io/validator/openapi.json")
-    println("file:\\\\\\$res")
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    val res = client.get("https://dataset.api.hub.geosphere.at/v1/station/current/tawes-v1-10min/metadata")
+    val metadata = res.body<JsonObject>()
+    val stations = metadata["stations"]?.jsonArray
+
+
+    val tl = client.get {
+        url("https://dataset.api.hub.geosphere.at/v1/station/current/tawes-v1-10min")
+        parameter("parameters", "TL")
+        for (station in stations!!) {
+            val stationId = station.jsonObject["id"]?.jsonPrimitive?.content
+            parameter("station_ids", stationId)
+        }
+    }.call.body<JsonObject>()
+
+    val features = tl["features"]?.jsonArray
+
+    for (property in features!!.map { it.jsonObject["properties"]?.jsonObject }) {
+        val stationId = property?.get("station")?.jsonPrimitive?.content
+        val temp = property?.get("parameters")?.jsonObject?.get("TL")?.jsonObject
+
+        val stationName = metadata["stations"]?.jsonArray?.find { it.jsonObject["id"]?.jsonPrimitive?.content == stationId }?.jsonObject?.get("name")?.jsonPrimitive?.content
+        println("Station $stationName has temperature $temp")
+    }
 }
