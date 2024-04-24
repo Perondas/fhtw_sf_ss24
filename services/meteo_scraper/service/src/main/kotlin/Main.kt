4db68c9136682eb7f5fddccfbb15a0a0cde0d459
Dominik Rauch <dominikrauch@gmx.net>
2024-04-24 16:51:40 +0200

    Adjusted sent meteo data.

--- services/meteo_scraper/service/src/main/kotlin/Main.kt
+++ services/meteo_scraper/service/src/main/kotlin/Main.kt
@@ -16,7 +16,9 @@ import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.*
-import java.util.*
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import kotlin.concurrent.timer
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -25,6 +27,8 @@ import kotlin.coroutines.suspendCoroutine
 data class Location(val zipCode: String, val name: String, val lat: Double, val lon: Double)
 
 const val TopicName = "meteo-data"
+
+val Formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
 
 suspend fun main() {
     val csv = object {}.javaClass.getResourceAsStream("/short-plz-coord-austria.csv")?.bufferedReader()?.readText()
@@ -35,13 +39,15 @@ suspend fun main() {
         Location(parts[0], parts[1], parts[3].toDouble(), parts[4].toDouble())
     }
 
-    val adminClient = configureAdminClient()
+    val props = createKafkaProperties()
+
+    val adminClient = AdminClient.create(props)
     if (!adminClient.listTopics().names().get().contains(TopicName)) {
         adminClient.createTopics(listOf(NewTopic(TopicName, 3, 2))).all().get()
     }
     adminClient.close()
 
-    val producer = configureProducer()
+    val producer = KafkaProducer<String, Weather> (props)
 
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
@@ -52,32 +58,48 @@ suspend fun main() {
     // Every 12 hours
     timer(period = 1000 * 60 * 60 * 12, daemon = true) {
         CoroutineScope(Dispatchers.IO).launch {
-            scrapeMeteo(locations, client, producer)
+            scrapeData(locations, client, producer)
         }
     }
 
     awaitCancellation()
 }
 
-private suspend fun scrapeMeteo(locations: List<Location>, client: HttpClient, producer: Producer<String, Weather>) {
-    for (location in locations) {
+private suspend fun scrapeData(locations: List<Location>, client: HttpClient, producer: Producer<String, Weather>) {
+    val currentTime = LocalDateTime.now()
+
+    for (location in locations.subList(0, 2)) { // TODO remove sublist - otherwise we might exceed api request limit
         val weatherResponse = retrieveWeather(client, location)
         println(weatherResponse)
 
-        val weather: Weather =
-            Weather.newBuilder()
-                .setLatitude(weatherResponse.latitude)
-                .setLongitude(weatherResponse.latitude)
-                .setTimezone(weatherResponse.timezone)
-                .setZipCode(location.zipCode)
-                .setRegion(location.name)
-                // TODO: Add the rest of the fields
-                .build()
+        val b = Weather.newBuilder()
 
-        // TODO: Add time of predictions in epoch seconds
-        val key = "meteo-${location.zipCode}-${Date().time}"
+        b.setLatitude(location.lat)
+        b.setLongitude(location.lon)
+        b.setRegion(location.name)
+        b.setZipCode(location.zipCode)
+        b.setTimezone(weatherResponse.timezone)
 
-        producer.asyncSend(ProducerRecord(TopicName, key, weather))
+        val hourly = weatherResponse.hourly
+        for ((index, time) in hourly.time.withIndex()) {
+            val timestamp = LocalDateTime.parse(time, Formatter)
+            if (timestamp.isBefore(currentTime)) {
+                continue
+            }
+            val epochSecond = timestamp.atOffset(ZoneOffset.UTC).toInstant().epochSecond
+
+            b.setTime(epochSecond)
+            b.setTemperature(hourly.temperature_2m[index])
+            b.setRelativeHumidity(hourly.relative_humidity_2m[index].toDouble())
+            b.setPrecipitation(hourly.precipitation[index])
+            b.setSurfacePressure(hourly.surface_pressure[index])
+
+            val data = b.build()
+            val key = "meteo-${location.zipCode}-${epochSecond}"
+
+            println(data)
+            producer.asyncSend(ProducerRecord(TopicName, key, data))
+        }
     }
 }
 
@@ -89,36 +111,23 @@ private suspend fun retrieveWeather(client: HttpClient, location: Location): Wea
         parameter("longitude", location.lon)
         parameter(
             "hourly",
-            "temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,soil_temperature_0cm"
+            "temperature_2m,relative_humidity_2m,precipitation,surface_pressure"
         )
         parameter("timezone", "Europe/Berlin")
+        parameter("forecast_days", "3")
     }
 
-    val weatherData = res.body<WeatherResponse>()
-
-    return weatherData
+    return res.body<WeatherResponse>()
 }
 
-private fun configureAdminClient(): AdminClient {
-    val props = mapOf(
-        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to (System.getenv("KAFKA_SERVER") ?: "localhost:9094"),
-        "security.protocol" to "PLAINTEXT",
-        "schema.registry.url" to (System.getenv("SCHEMA_SERVER") ?: "localhost:8081"),
-    )
-
-    return AdminClient.create(props)
-}
-
-private fun configureProducer(): Producer<String, Weather> {
-    val props = mapOf(
+private fun createKafkaProperties(): Map<String, String> {
+    return mapOf(
         ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to (System.getenv("KAFKA_SERVER") ?: "localhost:9094"),
         "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
         "value.serializer" to "io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer",
         "security.protocol" to "PLAINTEXT",
-        "schema.registry.url" to (System.getenv("SCHEMA_SERVER") ?: "localhost:8081"),
+        "schema.registry.url" to (System.getenv("SCHEMA_SERVER") ?: "http://localhost:8081"),
     )
-
-    return KafkaProducer(props)
 }
 
 private suspend fun <K, V> Producer<K, V>.asyncSend(record: ProducerRecord<K, V>) =
--- services/meteo_scraper/service/src/main/kotlin/model/WeatherClasses.kt
+++ services/meteo_scraper/service/src/main/kotlin/model/WeatherClasses.kt
@@ -1,4 +1,4 @@
-package model;
+package model
 
 import kotlinx.serialization.Serializable
 
@@ -7,19 +7,8 @@ data class HourlyUnits(
         val time: String,
         val temperature_2m: String,
         val relative_humidity_2m: String,
-        val apparent_temperature: String,
-        val precipitation_probability: String,
         val precipitation: String,
-        val rain: String,
-        val showers: String,
-        val snowfall: String,
-        val snow_depth: String,
-        val weather_code: String,
         val surface_pressure: String,
-        val cloud_cover: String,
-        val wind_speed_10m: String,
-        val wind_direction_10m: String,
-        val soil_temperature_0cm: String
 )
 
 @Serializable
@@ -27,19 +16,8 @@ data class Hourly(
         val time: List<String>,
         val temperature_2m: List<Double>,
         val relative_humidity_2m: List<Int>,
-        val apparent_temperature: List<Double>,
-        val precipitation_probability: List<Int>,
         val precipitation: List<Double>,
-        val rain: List<Double>,
-        val showers: List<Double>,
-        val snowfall: List<Double>,
-        val snow_depth: List<Double>,
-        val weather_code: List<String>,
         val surface_pressure: List<Double>,
-        val cloud_cover: List<Int>,
-        val wind_speed_10m: List<Double>,
-        val wind_direction_10m: List<Int>,
-        val soil_temperature_0cm: List<Double>
 )
 
 @Serializable
